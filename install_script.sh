3cb615c336b48031d44c2d7a02e31235ba9b01b3
NerdsCorp <86178816+NerdsCorp@users.noreply.github.com>
2024-08-20 14:31:48 -0500

    Update install_script.sh
    Revert changes

--- install_script.sh
+++ install_script.sh
@@ -6,7 +6,7 @@
 # Update package list and install required packages
 apt-get update -y
 apt-get install -y --no-install-recommends \
-    curl lib32gcc-s1 ca-certificates wget unzip x11vnc i3 xvfb mono-complete
+    curl lib32gcc1 ca-certificates wget unzip x11vnc i3 xvfb
 
 # Set default steam user if not provided
 if [ -z "${STEAM_USER}" ]; then
@@ -48,11 +48,11 @@ LOGS_DIR="/mnt/server/logs"
 mkdir -p "${CONFIG_DIR}/i3" "${CONFIG_DIR}/StardewValley" "${STORAGE_DIR}" "${MODS_DIR}" "${LOGS_DIR}"
 
 # Download and install SMAPI
-SMAPI_URL="https://github.com/Pathoschild/SMAPI/releases/download/4.0.8/SMAPI-4.0.8-installer.zip"
+SMAPI_URL="https://github.com/Pathoschild/SMAPI/releases/download/3.8/SMAPI-3.8.0-installer.zip"
 SMAPI_INSTALLER="${STORAGE_DIR}/nexus.zip"
 wget -qO "${SMAPI_INSTALLER}" "${SMAPI_URL}"
 unzip "${SMAPI_INSTALLER}" -d /mnt/server/nexus
-/bin/bash -c echo -e "2\n/mnt/server\n1\n" | mono /mnt/server/nexus/SMAPI 4.0.8 installer/internal/linux/SMAPI.Installer"
+echo -e "2\n/mnt/server\n1\n" | mono /mnt/server/nexus/SMAPI\ 3.8.0\ installer/internal/unix-install.exe
 
 # Download and set up configuration files
 CONFIG_BASE_URL="https://raw.githubusercontent.com/NerdsCorp/pelican-server-stardew-valley/main"
